5f38cb768110bcd500a80604b12c5955cf052944
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-08-27 14:23:58 -0700

    basic app set up done

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+
+const indexRouter = require("./routes/indexRouter");
+const path = require("node:path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use("/", indexRouter);
 
 const PORT = process.env.PORT || 3000;
 
-try {
-  app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}`);
-  });
-} catch (error) {
-  console.error("Error starting application: ", error);
-}
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Express app listening on port ${PORT}`);
+});
