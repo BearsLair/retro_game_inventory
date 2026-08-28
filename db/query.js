47e14a5e15b3b4047d1f7818ac5930703e94a2d3
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-08-27 21:41:51 -0700

    added query for adding games

--- db/query.js
+++ db/query.js
@@ -22,7 +22,35 @@ async function getGameDetails(stockID) {
   }
 }
 
+async function postGameDetails(
+  name,
+  console,
+  quantity,
+  price,
+  developer,
+  publisher,
+  genre,
+  releaseyear,
+) {
+  try {
+    const results = await pool.query(
+      "INSERT INTO stock (name, console, quantity, price) VALUES ($1, $2, $3, $4) RETURNING stockid;",
+      [name, console, quantity, price],
+    );
+    const id = result.rows[0].stockID;
+
+    await pool.query(
+      "INSERT INTO game_details (Developer, Publisher, Genre, ReleaseYear, stockID) VALUES ($1, $2, $3, $4)",
+      [developer, publisher, genre, releaseyear, id],
+    );
+    console.log("Game added to database.");
+  } catch (error) {
+    console.error("Error adding game details to db: ", error);
+  }
+}
+
 module.exports = {
   getAllStock,
   getGameDetails,
+  postGameDetails,
 };
